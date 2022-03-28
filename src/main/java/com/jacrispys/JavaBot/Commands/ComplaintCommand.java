58566a24f28913a205f90f8e6e00f8c589defd26
Jacrispys <jvanzyverden@icloud.com>
2022-03-28 11:23:23 -0700

    Cleaned up complaint options, and added Thread requests.

--- src/main/java/com/jacrispys/JavaBot/Commands/ComplaintCommand.java
+++ src/main/java/com/jacrispys/JavaBot/Commands/ComplaintCommand.java
@@ -1,6 +1,8 @@
 package com.jacrispys.JavaBot.Commands;
 
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.ThreadChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
@@ -11,24 +13,28 @@ import net.dv8tion.jda.api.interactions.components.Modal;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
-import net.dv8tion.jda.api.requests.restaction.MessageAction;
 import org.jetbrains.annotations.NotNull;
-;import javax.annotation.Nonnull;
+import org.yaml.snakeyaml.Yaml;
+
+import javax.annotation.Nonnull;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-public class ComplaintCommand extends ListenerAdapter  {
+public class ComplaintCommand extends ListenerAdapter {
 
     private static final Map<User, UUID> complaintId = new HashMap<>();
     private static final Map<User, User> complaintMention = new HashMap<>();
 
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if(event.getAuthor().isBot()) return;
-        if(event.isFromType(ChannelType.PRIVATE)) return;
-        if(!event.getMessage().getContentRaw().contains("!complaint")) {
-            if(event.getMessage().getContentRaw().contains("!clearcomplaint")) {
+        if (event.getAuthor().isBot()) return;
+        if (event.isFromType(ChannelType.PRIVATE)) return;
+        if (!event.getMessage().getContentRaw().contains("!complaint")) {
+            if (event.getMessage().getContentRaw().contains("!clearcomplaint")) {
                 complaintId.remove(event.getAuthor());
                 complaintMention.remove(event.getAuthor());
                 event.getMessage().reply("Complaint queue cleared!").queue(m -> m.delete().queueAfter(3, TimeUnit.SECONDS));
@@ -36,47 +42,93 @@ public class ComplaintCommand extends ListenerAdapter  {
             }
             return;
         }
-        if(event.getMessage().getMentionedUsers().size() != 1) return;
+        if (event.getMessage().getMentionedUsers().size() != 1) return;
         User mentionedUser = event.getMessage().getMentionedUsers().get(0);
         User sender = event.getAuthor();
-        if(mentionedUser.isBot()) {defendAllRobots(event); return;}
+        if (mentionedUser.isBot()) {
+            defendAllRobots(event);
+            return;
+        }
         UUID uuid = UUID.randomUUID();
-        Button button = Button.danger("complaint:" + uuid,"Create Complaint?");
-        if(complaintId.get(sender) != null || complaintMention.get(sender) != null) {
+        if (complaintId.get(sender) != null || complaintMention.get(sender) != null) {
             event.getMessage().reply("Cannot create a complaint when you already have an pending request! (Use !clearcomplaint to clear the request!)").queue(m -> m.delete().queueAfter(3, TimeUnit.SECONDS));
             event.getMessage().delete().queue();
             return;
         }
         complaintId.put(sender, uuid);
         complaintMention.put(sender, mentionedUser);
-
-        event.getMessage().reply("Click below to create a complaint!").setActionRow(button).queue();
+        Button button = Button.primary("complaint:" + uuid, "Create Complaint?");
+        Button openTicket = Button.primary("ticket:" + uuid, "Open a Ticket?");
+        Button cancelRequest = Button.danger("cancel:" + uuid, "Cancel?");
+        event.getMessage().reply("Click below to create a complaint!").setActionRow(button, openTicket, cancelRequest).queue();
         event.getMessage().delete().queue();
 
     }
 
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
-        if(event.getComponentId().equals("complaint:" + complaintId.get(event.getUser()))) {
-            TextInput reason = TextInput.create("Reason","Reasoning for complaint:", TextInputStyle.PARAGRAPH)
-                    .setMaxLength(500)
-                    .setRequired(true)
-                    .setPlaceholder("Reason:").build();
+        UUID buttonId = UUID.fromString(event.getComponentId().split(":")[1]);
+        if (buttonId.equals(complaintId.get(event.getUser()))) {
+            try {
+                switch (event.getComponentId().split(":")[0]) {
+                    case ("complaint"):
+                        TextInput reason = TextInput.create("Reason", "Reasoning for complaint:", TextInputStyle.PARAGRAPH)
+                                .setMaxLength(500)
+                                .setRequired(true)
+                                .setPlaceholder("Reason:").build();
 
 
-            Modal complaintModal = Modal.create("complaint:" + complaintId.get(event.getUser()), "User complaint (" +  complaintMention.get(event.getUser()).getAsTag() + "): ").addActionRows(ActionRow.of(reason)).build();
-            event.replyModal(complaintModal).queue();
-            event.getMessage().delete().queue();
+                        Modal complaintModal = Modal.create("complaint:" + complaintId.get(event.getUser()), "User complaint (" + complaintMention.get(event.getUser()).getAsTag() + "): ").addActionRows(ActionRow.of(reason)).build();
+                        event.replyModal(complaintModal).queue();
+                        event.getMessage().delete().queue();
+                        return;
+                    case ("ticket"):
+                        //
+                        Yaml yaml = new Yaml();
+                        InputStream is;
+                        try {
+                            is = getClass().getClassLoader().getResourceAsStream("guildData.yml");
+                            Map<String, Map<String, String>> values = yaml.load(is);
+                            if (values.containsKey(Long.parseLong(event.getGuild().getId()))) {
+                                Map<String, String> guildData = values.get(Long.parseLong(event.getGuild().getId()));
+                                String ticketChannel = guildData.get("tickets");
+                                TextChannel tickets = event.getGuild().getTextChannelById(ticketChannel);
+                                tickets.createThreadChannel(String.valueOf(buttonId), false).setAutoArchiveDuration(ThreadChannel.AutoArchiveDuration.TIME_1_HOUR).queue(threadChannel -> event.reply("Ticket opened here -> " + threadChannel.getAsMention()).setEphemeral(true).queue());
+
+                            }
+
+
+                        } catch (NullPointerException ex) {
+                            ex.printStackTrace();
+                        }
+
+                        event.getMessage().delete().queue();
+                        return;
+                    case ("cancel"):
+                        //
+                        event.reply("Request cancelled, to open a new complaint, just use !complaint and select a option!").setEphemeral(true).queue();
+                        event.getMessage().delete().queue();
+                        return;
+                    default:
+                        //
+                        event.reply("Could not locate the action requested... Please try again later!").setEphemeral(true).queue();
+                        event.getMessage().delete().queue();
+                }
+            } finally {
+                if (!(event.getComponentId().split(":")[0]).equalsIgnoreCase("complaint")) {
+                    complaintId.remove(event.getUser());
+                    complaintMention.remove(event.getUser());
+                }
+            }
         } else {
             event.reply("Only the user who issued the complaint can use this feature!").setEphemeral(true).queue();
         }
 
     }
-    public void onModalInteraction(@Nonnull ModalInteractionEvent event)
-    {
-        if (event.getModalId().equals("complaint:" + complaintId.get(event.getUser())))
-        {
-            String reason = event.getValue("Reason").getAsString();
+
+    public void onModalInteraction(@Nonnull ModalInteractionEvent event) {
+        if (event.getModalId().equals("complaint:" + complaintId.get(event.getUser()))) {
+            String reason = Objects.requireNonNull(event.getValue("Reason")).getAsString();
             User mentionedUser = complaintMention.get(event.getUser());
 
             collectComplaint(reason, event.getUser(), mentionedUser);
@@ -91,7 +143,7 @@ public class ComplaintCommand extends ListenerAdapter  {
     }
 
     protected void collectComplaint(String reason, User complainer, User complaint) {
-        complaint.openPrivateChannel().queue((privateChannel ->  {
+        complaint.openPrivateChannel().queue((privateChannel -> {
             privateChannel.sendMessage("User: " + complainer.getAsTag() + ", has complained about you with the following message: ").queue();
             privateChannel.sendMessage("> `" + reason + "`").queue();
         }));
