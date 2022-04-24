5fb40e4d35dfcdaca5b15e2a9ab00836212aa77e
Jacrispy <jvanzyverden@icloud.com>
2022-04-23 19:26:28 -0700

    WIP - Adding Message Filters

---
+++ src/main/java/dev/jacrispys/JavaBot/Events/WordBlocker.java
@@ -0,0 +1,24 @@
+package dev.jacrispys.JavaBot.Events;
+
+import dev.jacrispys.JavaBot.Utils.BlockedWords;
+import dev.jacrispys.JavaBot.Utils.MessageFilter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class WordBlocker extends ListenerAdapter {
+
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if(event.getAuthor().isBot()) return;
+        for(BlockedWords blocked : BlockedWords.values()) {
+            ArrayList<String> blockedWords = blocked.getList();
+            String message = event.getMessage().getContentRaw().toLowerCase(Locale.ROOT);
+            if(MessageFilter.filterMessage(message, blockedWords) != null) {
+                String wordBlocked = MessageFilter.filterMessage(message, blockedWords);
+                event.getMessage().reply("AYO?!? You really gonna say \"" + wordBlocked + "\" like its nothin??").queue();
+            }
+        }
+    }
+}
---
+++ src/main/java/dev/jacrispys/JavaBot/Utils/BlockedWords.java
@@ -0,0 +1,19 @@
+package dev.jacrispys.JavaBot.Utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public enum BlockedWords {
+    CURSES("FUCK","SHIT"),
+    OTHER("POOP");
+
+    final ArrayList<String> list = new ArrayList<>();
+    BlockedWords(String... s) {
+        this.list.addAll(Arrays.stream(s).toList());
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
+
+}
