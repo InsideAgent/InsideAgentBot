11363bebca03786cf0abe61da1a4be8d9b64e999
Jacrispys <jvanzyverden@icloud.com>
2022-08-31 15:01:28 -0700

    Lib Framework

---
+++ src/main/java/dev/jacrispys/JavaBot/api/analytics/GeneralJdaAnalytics.java
@@ -0,0 +1,4 @@
+package dev.jacrispys.JavaBot.api.analytics;
+
+public interface GeneralJdaAnalytics {
+}
---
+++ src/main/java/dev/jacrispys/JavaBot/api/analytics/GuildAnalytics.java
@@ -0,0 +1,4 @@
+package dev.jacrispys.JavaBot.api.analytics;
+
+public class GuildAnalytics implements UserAnalytics, AudioAnalytics, GeneralGuildAnalytics{
+}
