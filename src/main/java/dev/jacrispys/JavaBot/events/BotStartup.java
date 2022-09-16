1b518d6fe7009d08031937c58dadab34e2590fa8
Jacrispys <jvanzyverden@icloud.com>
2022-09-16 14:06:04 -0700

    Resolve merge error

--- src/main/java/dev/jacrispys/JavaBot/events/BotStartup.java
+++ src/main/java/dev/jacrispys/JavaBot/events/BotStartup.java
@@ -1,21 +1,15 @@
 package dev.jacrispys.JavaBot.events;
 
-import dev.jacrispys.JavaBot.commands.audio.SlashMusicCommands;
 import dev.jacrispys.JavaBot.commands.UnclassifiedSlashCommands;
-import dev.jacrispys.JavaBot.commands.debug.SlashDebugCommands;
+import dev.jacrispys.JavaBot.commands.audio.SlashMusicCommands;
 import dev.jacrispys.JavaBot.utils.mysql.MySQLConnection;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
