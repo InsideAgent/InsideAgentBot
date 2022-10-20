ada68c742ad60192e90b1814cad7552f894e10f2
Jacrispys <jvanzyverden@icloud.com>
2022-10-20 11:56:36 -0700

    Code Cleanup

--- src/main/java/dev/jacrispys/JavaBot/api/analytics/GuildAnalytics.java
+++ src/main/java/dev/jacrispys/JavaBot/api/analytics/GuildAnalytics.java
@@ -5,16 +5,11 @@ import dev.jacrispys.JavaBot.api.libs.AgentApi;
 
 public class GuildAnalytics extends GuildStats implements AudioAnalytics, GeneralGuildAnalytics {
 
-    private final long guildId;
-    private final AgentApi api;
-
-
     public GuildAnalytics(AgentApi api, long guildId) throws NullPointerException {
         super(guildId, api, api.getConnection().getJDA());
-        this.api = api;
-        if(validateGuild(guildId)) {
-            this.guildId = guildId;
-        } else throw new NullPointerException("Could not locate a guild with the given ID!");
+        if(!validateGuild(guildId)) {
+            throw new NullPointerException("Could not locate a guild with the given ID!");
+        }
     }
 
     private boolean validateGuild(long guildId) {
