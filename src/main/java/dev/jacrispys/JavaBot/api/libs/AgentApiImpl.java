1f5f6b0064b63d8045aa24e046d364963481cb8e
Jacrispys <jvanzyverden@icloud.com>
2023-02-01 10:11:55 -0800

    All API packages documented

--- src/main/java/dev/jacrispys/JavaBot/api/libs/AgentApiImpl.java
+++ src/main/java/dev/jacrispys/JavaBot/api/libs/AgentApiImpl.java
@@ -8,6 +8,9 @@ import dev.jacrispys.JavaBot.api.libs.auth.DeveloperConnection;
 import dev.jacrispys.JavaBot.api.libs.auth.UserConnection;
 import dev.jacrispys.JavaBot.api.libs.utils.AgentOptions;
 
+/**
+ * Implementation of all methods from {@link AgentApi}
+ */
 public class AgentApiImpl implements AgentApi {
 
     private UserConnection connection = null;
@@ -27,55 +30,33 @@ public class AgentApiImpl implements AgentApi {
         this.devToken = true;
     }
 
-
-    /**
-     * @return
-     */
     @Override
     public UserConnection getUserConnection() throws NullPointerException {
         if(connection == null) throw new NullPointerException("Cannot obtain user connection when using a dev key, Idiot.");
         return connection;
     }
 
-    /**
-     * @return
-     */
     @Override
     public DeveloperConnection getDevConnection() {
         if(devConnection == null) throw new NullPointerException("Cannot obtain developer connection without dev key!");
         return devConnection;
     }
 
-    /**
-     * @return
-     */
     @Override
     public ClientConnection getConnection() {
         return connection != null ? connection : devConnection;
     }
 
-    /**
-     * @return
-     */
     @Override
     public boolean isDevAccount() {
         return devToken;
     }
 
-    /**
-     * @param api
-     * @param guildId
-     * @return
-     */
     @Override
     public GuildAnalytics getGuildAnalytics(AgentApi api, long guildId) {
         return new GuildAnalytics(api, guildId);
     }
 
-    /**
-     * @param userId
-     * @return
-     */
     @Override
     public JdaUser getJdaUser(long userId) {
         return new JdaUserImpl(getConnection().getJDA(), userId);
